f5dbad7c7beaa84118b77a8a60aac6c484042a56
Dylan Martin <66983783+foolofmyself@users.noreply.github.com>
2023-09-21 11:01:00 -0400

    adding auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-
+import React, { useState, useEffect} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import "@aws-amplify/ui-react/styles.css";
+import security from "./assets/security.jpg";
 
 import Home from './routes/Home';
 import Pricing from './routes/Pricing'
 // import Register from './routes/auth/RegisterPage';
 
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-
-function App() {
+function App({ signOut }) {
   return (
-     <>
-      <Routes>
+     <><View className="App">
+      <Card>
+        <Image src={security} className="App-logo" alt="logo" />
+        <Heading level={1}>we now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View><Routes>
         <Route path='/' element={<Home />} />
-      </Routes>
-    </>
+      </Routes></>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+import "./App.css";
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <BrowserRouter>
